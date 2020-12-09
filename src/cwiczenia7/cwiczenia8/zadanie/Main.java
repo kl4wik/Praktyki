c3be85141ca2a0e2a283eb21239a47d99d94f454
Grabsky <grabskypl@gmail.com>
2020-12-09 14:00:51 +0100

    zadanie

---
+++ src/cwiczenia7/cwiczenia8/zadanie/Main.java
@@ -0,0 +1,8 @@
+package cwiczenia7.cwiczenia8.zadanie;
+
+public class Main {
+    public static void main(String[] args) {
+        Moneta moneta = new Moneta();
+        System.out.println(moneta.rzut());
+    }
+}
---
+++ src/cwiczenia7/cwiczenia8/zadanie/Moneta.java
@@ -0,0 +1,25 @@
+package cwiczenia7.cwiczenia8.zadanie;
+
+import java.security.SecureRandom;
+
+public class Moneta {
+    private final int liczbaStron;
+    private final SecureRandom random;
+
+    public Moneta() {
+        liczbaStron = 2;
+        random = new SecureRandom();
+    }
+
+    public String rzut() {
+        int i = random.nextInt(liczbaStron);
+        switch(i) {
+            case 0:
+                return "Orzeł";
+            case 1:
+                return "Reszka";
+            default:
+                return "";
+        }
+    }
+}
